d128385ac59f74e21b12865159f6ce62e533c69e
Tobias Herrmann <tobias@twinfive.com>
2023-03-16 18:31:12 +0100

    d

--- countrywrangler/databases/names.py
+++ countrywrangler/databases/names.py
@@ -19,7 +19,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-class CountryName:
+class CountryNames:
 
     def to_alpha2():
         return {
--- countrywrangler/normalize.py
+++ countrywrangler/normalize.py
@@ -19,7 +19,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-from .databases.names import CountryName
+from databases.names import CountryNames
 
 
 class Normalize:
@@ -39,7 +39,7 @@ class Normalize:
         # Convert to lower case according to database records and remove whitespace
         text = text.lower().strip()
         # Database lookup
-        names = CountryName.to_alpha2()
+        names = CountryNames.to_alpha2()
         if text in names:
             return names[text]
         else:
