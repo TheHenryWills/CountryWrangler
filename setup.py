0cbec0e5573d656077c438f3c13fc84cc8b42267
Tobias Herrmann <tobias@twinfive.com>
2023-03-16 17:31:41 +0100

    update

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'countrywrangler',        
   packages = ['countrywrangler'],  
-  version = '0.0.1',     
+  version = '0.0.2',     
   license='MIT',       
   description = 'A library that simplifies the handling of country-related data. Easily standardize your data and make it consistent across your project.', 
   author = 'Henry Wills',                   
   author_email = 'hello@henrywills.com',      
   url = 'https://github.com/TheHenryWills/CountryWrangler', 
-  download_url = 'https://github.com/TheHenryWills/CountryWrangler/archive/refs/tags/v_001.tar.gz', 
+  download_url = 'https://github.com/TheHenryWills/CountryWrangler/archive/refs/tags/v_002.tar.gz', 
   keywords = ['iso-3166', 'iso-3166-1 ', 'normalize', ' countries-data', 'country', 'data-normalization', 'data-cleaning'],  
   install_requires=[            # I get to this in a second
           'phone_iso3166',
