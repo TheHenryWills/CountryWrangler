1bf1361aee26fc22c851f4c203eaa01bd642ada5
Tobias Herrmann <tobias@twinfive.com>
2023-03-16 18:59:32 +0100

    sdf

--- countrywrangler/normalize.py
+++ countrywrangler/normalize.py
@@ -19,7 +19,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-from databases.names import CountryNames
+from countrywrangler.databases.names import CountryNames
 
 
 class Normalize:
