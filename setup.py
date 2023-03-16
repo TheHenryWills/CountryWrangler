8c720a54239ebc62c2090f1254fcea9d04db50e2
Tobias Herrmann <tobias@twinfive.com>
2023-03-16 18:54:35 +0100

    g

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(filename):
 
 setup(
   name = 'countrywrangler',        
-  packages = ['countrywrangler'],  
+  packages = ['countrywrangler', 'databases', 'databases.names'],  
   version = '0.1.2',     
   license='MIT',       
   description = 'A library that simplifies the handling of country-related data. Easily standardize your data according to ISO 3166-1 and make it consistent across your project.', 
@@ -20,7 +20,6 @@ setup(
   url = 'https://github.com/TheHenryWills/CountryWrangler', 
   download_url = 'https://github.com/TheHenryWills/CountryWrangler/archive/refs/tags/v_012.tar.gz', 
   keywords = ['iso-3166', 'iso-3166-1 ', 'normalize', ' countries-data', 'country', 'data-normalization', 'data-cleaning'],  
-  packages=['databases', 'databases.names'],
   install_requires=[           
           'phone_iso3166',
       ],
