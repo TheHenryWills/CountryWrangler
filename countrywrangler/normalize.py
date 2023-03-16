65751b0200021478f44f5f7ece1366c609b72eb6
Tobias Herrmann <tobias@twinfive.com>
2023-03-16 17:38:22 +0100

    f

--- countrywrangler/normalize.py
+++ countrywrangler/normalize.py
@@ -19,7 +19,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-from .databases.name_to_alpha2 import CountryName
+from .databases.names import CountryName
 
 
 class Normalize:
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'countrywrangler',        
   packages = ['countrywrangler'],  
-  version = '0.0.2',     
+  version = '0.0.3',     
   license='MIT',       
   description = 'A library that simplifies the handling of country-related data. Easily standardize your data and make it consistent across your project.', 
   author = 'Henry Wills',                   
   author_email = 'hello@henrywills.com',      
   url = 'https://github.com/TheHenryWills/CountryWrangler', 
-  download_url = 'https://github.com/TheHenryWills/CountryWrangler/archive/refs/tags/v_002.tar.gz', 
+  download_url = 'https://github.com/TheHenryWills/CountryWrangler/archive/refs/tags/v_003.tar.gz', 
   keywords = ['iso-3166', 'iso-3166-1 ', 'normalize', ' countries-data', 'country', 'data-normalization', 'data-cleaning'],  
   install_requires=[           
           'phone_iso3166',
